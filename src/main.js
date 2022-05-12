2e5ae56159b8a5813e210edb47f19e2730f899be
Evgeny Fedorenko <evgeny.fedorenko@constant.obninsk.ru>
2022-05-12 16:31:27 +0300

    feat(PWA):PWA was created

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import store from "./store";
-import "./plugins/bootstrap";
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import './plugins/bootstrap';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
